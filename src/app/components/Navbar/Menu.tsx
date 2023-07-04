49d1d61685e289f303d80ee04e485f3d6a8f8fa5
arinugraha11 <ari.nugraha@tokopedia.com>
2023-07-04 23:17:55 +0700

    feat: create web navbar

---
+++ src/app/components/Navbar/Menu.tsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <div className="flex gap-5">
+      <h4 className="text-gray-500">About</h4>
+      <h4 className="text-gray-500">Pricing</h4>
+      <h4 className="text-gray-500">Event</h4>
+    </div>
+  );
+};
+
+export default Menu;
--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Menu from "./Menu";
+import BtnAppointment from "./BtnAppointment";
+
 const Navbar = () => {
-  return <div>Ini bakalan jadi navbar</div>;
+  return (
+    <div className="flex w-screen justify-between h-20 items-center border-b px-24">
+      <Image
+        src="/assets/main-logo.png"
+        alt="main-logo"
+        width={110}
+        height={50}
+      />
+      <Menu />
+      <BtnAppointment />
+    </div>
+  );
 };
 
 export default Navbar;
