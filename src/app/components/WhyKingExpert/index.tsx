0405cd03dacf388beef7f5c8e1ca33f1be527359
arinugraha11 <ari.nugraha@tokopedia.com>
2023-07-07 00:07:57 +0700

    feat: change pengalaman

--- src/app/components/WhyKingExpert/index.tsx
+++ src/app/components/WhyKingExpert/index.tsx
@@ -1,19 +1,23 @@
-import { WHY_KING } from "@/app/constants";
-import Chart from "./Chart";
-
 const WhyKingExpert = () => {
   return (
-    <div className="flex w-screen px-24">
-      <div className="flex flex-col flex-1 gap-8">
-        <p className="text-sm text-justify max-w-lg">{WHY_KING}</p>
-
-        <Chart />
+    <div className="flex justify-center w-screen px-24 gap-10">
+      <div className="flex gap-1 flex-col bg-mainOrange rounded-2xl w-36 h-36 justify-center items-center">
+        <h2 className="h-fit color text-white font-bold text-4xl">20+</h2>
+        <p className="h-fit text-white text-sm">Tutor</p>
       </div>
 
-      <div className="flex items-start justify-start w-5/12 -ml-10">
-        <h2 className="text-6xl font-bold text-right">
-          Kenapa <br /> King Expert?
-        </h2>
+      <div className="h-36 w-0.5 bg-black " />
+
+      <div className="flex gap-1 flex-col bg-mainOrange rounded-2xl w-36 h-36 justify-center items-center">
+        <h2 className="h-fit color text-white font-bold text-4xl">50+</h2>
+        <p className="h-fit text-white text-sm">Mitra</p>
+      </div>
+
+      <div className="h-36 w-0.5 bg-black " />
+
+      <div className="flex gap-1 flex-col bg-mainOrange rounded-2xl w-36 h-36 justify-center items-center">
+        <h2 className="h-fit color text-white font-bold text-4xl">500+</h2>
+        <p className="h-fit text-white text-sm">Siswa</p>
       </div>
     </div>
   );
