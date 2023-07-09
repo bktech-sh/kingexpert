6691699459e750a9537249cc03ad0dce106207c4
arinugraha11 <ari.nugraha@tokopedia.com>
2023-07-09 10:01:33 +0700

    feat: add helper is android

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Customers from "./components/Customers";
 import WhyKingExpert from "./components/WhyKingExpert";
 import MetodeBelajar from "./components/MetodeBelajar";
 import Footer from "./components/Footer";
+import useIsAndroid from "./helpers/useIsAndroid";
+import { useEffect } from "react";
 
 export default function Home() {
+  const isAndroid = useIsAndroid();
+
+  useEffect(() => {
+    console.log("999 is android", isAndroid);
+  }, [isAndroid]);
+
   return (
     <main className="flex flex-col w-screen">
       <Navbar />
