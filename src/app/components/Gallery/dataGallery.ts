3492efbabd635eed2b3a16ed2af1402c2a22858e
arinugraha11 <ari.nugraha@tokopedia.com>
2023-07-13 22:55:27 +0700

    feat: change image

--- src/app/components/Gallery/dataGallery.ts
+++ src/app/components/Gallery/dataGallery.ts
@@ -1,9 +1,8 @@
 export const dataGallery = [
-  "/images/gallery-1.png",
-  "/images/gallery-2.png",
-  "/images/gallery-3.png",
-  "/images/gallery-4.png",
-  "/images/gallery-5.png",
-  "/images/gallery-6.png",
-  "/images/gallery-7.png",
+  "/images/gallery-11.png",
+  "/images/gallery-22.png",
+  "/images/gallery-33.png",
+  "/images/gallery-44.png",
+  "/images/gallery-55.png",
+  "/images/gallery-66.png",
 ];
